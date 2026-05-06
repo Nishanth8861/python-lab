1b2640384068b664e3302dd66d8709b5c23c7674
Nishanth8861 <nishanthshetty8861@gmail.com>
2026-05-06 18:40:01 +0530

    Create output 9a.py

---
+++ Nishanth V Shetty Python Lab/output 9a.py
@@ -0,0 +1,23 @@
+1.Add  2.Display  3.Topper  4.Exit
+Enter choice: 1
+Enter name: Rahul
+Enter marks: 85
+Student added!
+
+1.Add  2.Display  3.Topper  4.Exit
+Enter choice: 1
+Enter name: Priya
+Enter marks: 92
+Student added!
+
+1.Add  2.Display  3.Topper  4.Exit
+Enter choice: 2
+Rahul -> 85
+Priya -> 92
+
+1.Add  2.Display  3.Topper  4.Exit
+Enter choice: 3
+Topper: Priya with 92 marks
+
+1.Add  2.Display  3.Topper  4.Exit
+Enter choice: 4
